5fdd47cc69d556f85a30dbaae88f6a0bf730eda0
SupEric <[zcr1066414992@163.com]>
2022-07-11 21:55:52 +0800

    22/7/11

--- DLSC/WISTA.py
+++ DLSC/WISTA.py
@@ -1,41 +1,71 @@
+from unittest import FunctionTestCase
+from cv2 import PSNR
 import numpy as np
 import matplotlib.pyplot as plt
-import geneData
+import cv2
 
+from function import Function
 from Info import Info
 from Record import Record
 
-info = Info()
-Record = Record()
-[X, D] = geneData.GeneData(info, Record)
-lamda = 200e-0
-alpha = 100
-maxLayer = 5
+class WISTA:
 
-def forwardP(X, D, lamda, alpha, T):
-    [m, n] = np.shape(D)
-    [M, N] = np.shape(X)
-    p = 2
-    I = np.eye(n)
-    DT = D.T
-    
-    t = lamda/alpha
-    z = np.zeros((n,N))
-    H = I - np.dot(DT, D)/alpha
-    W = DT / alpha
-    B = np.dot(W,X)
-    
-    for k in range(1, T):
-        c = B + np.dot(H,z)
-        z = np.sign(c)*np.maximum(np.abs(c)-t,0)
-        t = lamda/alpha * np.abs(z)**(p-1)
+    info = Info()
+    record = Record()
+    X0 = cv2.imread("AP_gray.png", 0).astype(float) #读取图片 512×512
+    sigma = 20 #噪声强度
+    X1 = Function.addNoise(X0, sigma, record)
+    PSNR = Function.calculate_PSNR(X0,X1)
+    X, D = Function.GeneData(X0, X1, info)
+    lamda = 1e-5
+    maxLayer = 100
+    alpha = 15
+
+    def forwardP(X, D, lamda, alpha, T):
+        m, n = np.shape(D)
+        M, N = np.shape(X)
+        p = 0.7
+        I = np.eye(n)
+        DT = D.T
+        eigenvalue, featurevector = np.linalg.eig(np.dot(DT,D))
         
-    return z
+        t = lamda/alpha
+        z = np.zeros((n,N))
+        H = I - np.dot(DT, D)/alpha
+        W = DT / alpha
+        B = np.dot(W,X)
+        
+        for k in range(0, T):
+            # B = np.dot(D,z) - X
+            # c = z - 1/alpha * np.dot(DT,B)
+            # z = np.sign(c)*np.maximum(np.abs(c)-t, 0)
+            # t = (lamda/alpha) * (np.abs(z)**(p-1))
+            
+            c = B + np.dot(H,z)
+            z = np.sign(c)*np.maximum(np.abs(c)-t,0)
+            t = lamda/alpha * np.abs(z)**(p-1)
+            
+            HP = Function.calculate_HoyerSparsity(z)
+            print('iter:',k,'Hoyer Sparity:',HP)
+           
+            
+        return z, D
     
-def backwardP():
-    return
+    def backwardP(C, Z):
+        return
 
-z = forwardP(X, D, lamda, alpha, maxLayer)
-X1 = np.dot(D,z)
-for i in range(0,np.size(X,1)):
-    X[i].reshape()
+    z, D = forwardP(X, D, lamda, alpha, maxLayer)
+    X2 = np.dot(D,z)
+    Img = Function.recoverImg(X2,512,8)
+    PSNR1 = Function.calculate_PSNR(X0,Img)
+    plt.figure()
+    plt.subplot(1, 3, 1)
+    plt.imshow(X0, cmap='gray')
+    plt.title('Origion image')
+    plt.subplot(1, 3, 2)
+    plt.imshow(X1, cmap='gray')
+    plt.title('Noisy image, PSNR:%f' %PSNR)
+    plt.subplot(1, 3, 3)
+    plt.imshow(Img, cmap='gray')
+    plt.title('Recover image, PSNR:%f' %PSNR1)
+    plt.show()
---
+++ DLSC/function.py
@@ -0,0 +1,128 @@
+# coding=utf-8
+import numpy as np
+import matplotlib.pyplot as plt
+
+from Info import Info
+from Record import Record
+
+class Function:
+    def addNoise(y0, sigma, Record):
+        N = np.size(y0,0) #512
+        noise = abs(np.random.randn(N, N)) #随机生成高斯噪声
+        y1 = y0 + sigma * noise #添加噪声
+        Record.PSNRinpuut = 10 * np.log10(255**2 / np.mean(np.power((y1[:] - y0[:]), 2))) #计算PSNR峰值信噪比
+        return y1
+        
+    def GeneData(y0, y1, info):
+        m,n = 8,16
+        #初始化字典
+        Dictionary = np.zeros((m,n))
+        for k in range(0, n):
+            V = np.cos(np.arange(0,m)*np.pi*k/n)
+            if k > 0:
+                V = V - np.mean(V)
+            Dictionary[:, k] = V/np.linalg.norm(V)
+
+        Dictionary = np.kron(Dictionary, Dictionary)
+        Dictionary = np.dot(Dictionary, np.diag(1/np.sqrt(np.sum(Dictionary * Dictionary, 0))))
+        NoTotal = np.size(y1, 0) - m + 1
+        cnt, Kdata = 0, 8
+        sidenum = int(np.ceil((NoTotal-1)/Kdata)+1)
+        Data = np.zeros((m**2, sidenum**2))
+        Datanorm = np.ones((1, sidenum**2))
+
+        if np.mod((NoTotal-1), Kdata) == 0:
+            for j in range(0, NoTotal, Kdata):
+                for i in range(0, NoTotal, Kdata):
+                    patch = y1[i:i+m,j:j+m].reshape(m**2)
+                    Data[:, cnt] = patch[:]
+                    cnt = cnt + 1
+        else:
+            i,j = 0
+            for k in range(0,sidenum**2):
+                patch = y1[i:i+m, j:j+m].reshape(m**2)
+                Data[:, k] = patch[:]
+                if i<np.size(y1,0)-2*m:
+                    i = i+Kdata
+                elif i<np.size(y1,0)-m:
+                    i = np.size(y1,0) - m
+                else:
+                    i = 1
+                    if j<np.size(y1,0)-2*m:
+                        j=j+Kdata
+                    else: 
+                        j=np.size(y1,0)-m
+
+        if info.paralltrigger:
+            if info.parallcase == 'aver':
+                Data = Data - np.sum(Data) / (m**2*np.size(Data,1))
+
+        if info.normtrigger:
+            for i in range(0, np.size(Data, 1)):
+                Datanorm[:,i] = np.linalg.norm(Data[:,i])
+                Data[:,i] = patch[:]/Datanorm[:,i]
+
+        NoTotal = np.size(y1,0)-m+1
+        cnt, Kdata1 = 0,1
+        Data1 = np.zeros((m**2,int((NoTotal-1)/Kdata1+1)**2))
+        Data1norm = np.zeros((1,int((NoTotal-1)/Kdata1+1)**2))
+        for j in range(0, NoTotal, Kdata1):
+            for i in range(0, NoTotal, Kdata1):
+                patch = y1[i:i+m,j:j+m].reshape(m**2)
+                Data1[:,cnt] = patch[:]
+                #DataZ(:,:,cnt)=patch(:,:);
+                cnt += 1
+
+        if info.paralltrigger:
+            if info.parallcase == 'aver':
+                Data1 = Data1 - np.sum(Data1) / (m**2*np.size(Data1,1))
+
+        if info.normtrigger:
+            for i in range(0, np.size(Data1, 1)):
+                Data1norm[:,i] = np.linalg.norm(Data1[:,i])
+                Data1[:,i] = patch[:]/Data1norm[:,i]
+
+        PSNRinpuut = 10 * np.log10(255**2 / np.mean(np.power((y1[:] - y0[:]), 2)))
+        
+        # plt.figure()
+        # plt.subplot(1, 2, 1)
+        # plt.imshow(y0, cmap='gray')
+        # plt.title('Original image')
+        # plt.subplot(1, 2, 2)
+        # plt.imshow(y1, cmap='gray')
+        # plt.title('Add noise: PSNR=%f' %PSNRinpuut)
+        # plt.show()
+        
+        return Data1, Dictionary
+
+    def recoverImg(Data, imgSize, k):
+        img = np.zeros((imgSize,imgSize))
+        cnt = 0
+        t = imgSize-k+1
+        for j in range(0, t, 1):
+            for i in range(0, t, 1):
+                patch = Data[:,cnt].reshape(k,k)
+                img[i:i+k, j:j+k] = patch
+                cnt += 1
+        return img
+    
+    def calculate_PSNR(X0, X1):
+        PSNR = 10 * np.log10(255**2 / np.mean(np.power((X1[:] - X0[:]), 2)))
+        return PSNR
+    
+    def calculate_HoyerSparsity(Z):
+        sqrtn = np.sqrt(np.size(Z))
+        norm = np.sum(np.abs(Z)) / np.sqrt(np.sum(Z**2))
+        HP = (sqrtn-norm) / (sqrtn-1)
+        return HP
+        
+
+    if __name__ == "__main__":
+        info = Info()
+        Record = Record()
+        Data,X = GeneData(info, Record)
+        Img = recoverImg(Data,512,8)
+        
+        plt.imshow(Img, cmap="gray")
+        plt.title("Recover Image: Arthur")
+        plt.show()
